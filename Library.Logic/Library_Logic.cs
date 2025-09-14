68d67c669713ff90916053551c728a825e10922a
Alekcey7795 <alekcey7795@mail.ru>
2025-09-14 19:05:52 +0700

    Доработка логики и добавление фильтрации

--- Library.Logic/Library_Logic.cs
+++ Library.Logic/Library_Logic.cs
@@ -90,6 +90,39 @@ namespace Library.Logic
             var book = new List<Book>();
             using var conn = _context.GetConnection();
             using var cmd = new NpgsqlCommand("SELECT * FROM books", conn);
+
+            using var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                book.Add(MapBook(reader));
+            }
+
+            return book;
+        }
+
+        public List<Book> GetBooksByYearRange(int data)
+        {
+            var book = new List<Book>();
+            using var conn = _context.GetConnection();
+            using var cmd = new NpgsqlCommand("SELECT * FROM books WHERE year_create = @data", conn);
+
+            using var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                book.Add(MapBook(reader));
+            }
+
+            return book;
+        }
+
+        public List<Book> GetCategoryByTitle(int id)
+        {
+            var book = new List<Book>();
+            using var conn = _context.GetConnection();
+            using var cmd = new NpgsqlCommand("SELECT * FROM books WHERE category_id = @id", conn);
+
             using var reader = cmd.ExecuteReader();
 
             while (reader.Read())
@@ -124,7 +157,7 @@ namespace Library.Logic
                 Author = reader.GetString("author"),
                 Number_of_pages = reader.GetInt32("number_of_pages"),
                 Year_Create = reader.GetInt32("year_create"),
-                CategoryId = reader.GetInt32("category"),
+                CategoryId = reader.GetInt32("category_id"),
             };
         }
 
@@ -136,5 +169,63 @@ namespace Library.Logic
                 Title = reader.GetString("title")
             };
         }
+
+        public bool UpdateBook(int id,
+            string? title = null,
+            string? description = null,
+            string? author = null,
+            int? numberOfPages = null,
+            int? yearCreate = null,
+            int? categoryId = null)
+        {
+            using var conn = _context.GetConnection();
+
+            var sql = "UPDATE books SET ";
+            var parameters = new List<NpgsqlParameter>();
+            var setParts = new List<string>();
+
+            if (title != null)
+            {
+                setParts.Add("title = @title");
+                parameters.Add(new NpgsqlParameter("title", title));
+            }
+            if (description != null)
+            {
+                setParts.Add("description = @description");
+                parameters.Add(new NpgsqlParameter("description", description));
+            }
+            if (author != null)
+            {
+                setParts.Add("author = @author");
+                parameters.Add(new NpgsqlParameter("author", author));
+            }
+            if (numberOfPages != null)
+            {
+                setParts.Add("number_of_pages = @number_of_pages");
+                parameters.Add(new NpgsqlParameter("number_of_pages", numberOfPages.Value));
+            }
+            if (yearCreate != null)
+            {
+                setParts.Add("year_create = @year_create");
+                parameters.Add(new NpgsqlParameter("year_create", yearCreate.Value));
+            }
+            if (categoryId != null)
+            {
+                setParts.Add("category_id = @category_id");
+                parameters.Add(new NpgsqlParameter("category_id", categoryId.Value));
+            }
+
+            if (setParts.Count == 0) return false;
+
+            sql += string.Join(", ", setParts) + " WHERE id = @id";
+            parameters.Add(new NpgsqlParameter("id", id));
+
+            using var cmd = new NpgsqlCommand(sql, conn);
+            cmd.Parameters.AddRange(parameters.ToArray());
+
+            return cmd.ExecuteNonQuery() > 0;
+        }
+
+        
     }
 }
