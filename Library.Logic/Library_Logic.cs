4db61ce3a416763505c9cd12e80d389993ae903c
Alekcey7795 <alekcey7795@mail.ru>
2025-09-14 15:28:58 +0700

    Создание методов CreateBook, CreateCategory и их реализация

--- Library.Logic/Library_Logic.cs
+++ Library.Logic/Library_Logic.cs
@@ -1,9 +1,65 @@
-﻿namespace Library.Logic
+﻿using Library.Model;
+using Npgsql;
+
+namespace Library.Logic
 {
     public class Library_Logic
     {
+        private readonly PostgresContext _context;
 
+        public Library_Logic()
+        {
+            var connectionString = "Host=localhost;Port=5432;Database=TaskManagerDB;Username=postgres;Password=твой_пароль";
+            _context = new PostgresContext(connectionString);
 
+        }
 
+        public Book CreateBook(string title, string description, string author, int numberOfPages, int yearCreate, string categoryId)
+        {
+            using var conn = _context.GetConnection();
+            using var cmd = new NpgsqlCommand(@"
+                INSERT INTO books (title, description, author, number_of_pages, year_create, category_id)
+                VALUES (@title, @description, @author, @number_of_pages, @year_create, @category_id)
+                RETURNING id", conn);
+
+            cmd.Parameters.AddWithValue("title", title);
+            cmd.Parameters.AddWithValue("description", description);
+            cmd.Parameters.AddWithValue("author", author);
+            cmd.Parameters.AddWithValue("number_of_pages", numberOfPages);
+            cmd.Parameters.AddWithValue("year_create", yearCreate);
+            cmd.Parameters.AddWithValue("category_id", categoryId);
+
+            var id = Convert.ToInt32(cmd.ExecuteScalar());
+
+            return new Book
+            {
+                Id = id,
+                Title = title,
+                Description = description,
+                Author = author,
+                Number_of_pages = numberOfPages,
+                Year_Create = yearCreate,
+                CategoryId = categoryId
+            };
+        }
+
+        public Category CreateCategory(string title)
+        {
+            using var conn = _context.GetConnection();
+            using var cmd = new NpgsqlCommand(@"
+                INSERT INTO books (title)
+                VALUES (@title)
+                RETURNING id", conn);
+
+            cmd.Parameters.AddWithValue("title", title);
+
+            var id = Convert.ToInt32(cmd.ExecuteScalar());
+
+            return new Category
+            {
+                Id = id,
+                Title = title,
+            };
+        }
     }
 }
--- Library.Model/Book.cs
+++ Library.Model/Book.cs
@@ -4,18 +4,16 @@
     {
         public int Id { get; set; }
 
-        public string Title { get; set; }
+        public string Title { get; set; } = string.Empty;
 
-        public string Description { get; set; }
+        public string Description { get; set; } = string.Empty;
 
-        public string Author { get; set; }
+        public string Author { get; set; } = string.Empty;
          
         public int Number_of_pages { get; set; }
 
         public int Year_Create { get; set; }
 
-        public string Category { get; set; }
-
-
+        public string CategoryId { get; set; } = string.Empty;
     }
 }
