75d26527d0ac89f64025060a0ddb261eafd73f28
Alekcey7795 <alekcey7795@mail.ru>
2025-09-14 15:07:07 +0700

    Создание класса PostgresContext и его реализация

---
+++ Library.Logic/PostgresContext.cs
@@ -0,0 +1,26 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Logic
+{
+    public class PostgresContext
+    {
+        private readonly string _connectionString;
+
+        public PostgresContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public NpgsqlConnection GetConnection()
+        {
+            var connection = new NpgsqlConnection(_connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
